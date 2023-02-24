a729b269a2941c71763471f4620b64e92b627001
dbrudsodlf <contiki0527@naver.com>
2023-02-24 22:14:21 +0900

    js_calculator

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert('hi');
+
+
+const calculator={
+    add: function(a,b){
+        console.log(a+b);
+    },
+
+    min: function(a,b){
+        console.log(a-b);
+    },
+
+    div: function(a,b){
+        console.log(a/b);
+    },
+
+    pow: function(a,b){
+        console.log(a**b);
+    }
+}
+
+
+calculator.add(10,5);
+calculator.min(10,5);
+calculator.div(100,2);
+calculator.pow(5,2);
