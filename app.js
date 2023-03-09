c0d2ae9f8a0423446b8a2e268a3e20014cd6fcb1
dbrudsodlf <contiki0527@naver.com>
2023-03-09 22:26:04 +0900

    hello yk

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput=document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault(); //자동 저장을 막아주는 라인
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    //greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; //'' ""아니고 백틱임 ``
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit",onLoginSubmit)
+loginForm.addEventListener("submit", onLoginSubmit)
