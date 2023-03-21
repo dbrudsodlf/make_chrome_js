86d7ffffed644154fd74d3a09f3800b4ed141047
dbrudsodlf <contiki0527@naver.com>
2023-03-21 23:37:14 +0900

    todos1

---
+++ js/todo.js
@@ -0,0 +1,41 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const todoList = document.getElementById("todo-list");
+
+const TODOS_KEY ="todos";
+
+const toDos=[];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText=newTodo;
+    const button = document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click",deleteToDo);
+    li.appendChild(span); //span을 li 안으로 넣기
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value=""; //엔터시 칸 비우기
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const saveToDos = localStorage.getItem(TODOS_KEY);
