68e917ce7c1c3712614428fdc845939452dd9190
dbrudsodlf <contiki0527@naver.com>
2023-03-24 23:45:43 +0900

    weather1

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["dawn.jpg","camel.jpg","uk.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src =`img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+    
--- js/greetings.js
+++ js/greetings.js
@@ -1,27 +1,27 @@
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); //자동 저장을 막아주는 라인
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.ariaValueMax;
+    const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY,username)
     //greeting.innerText = "Hello " + username;
     paintGreetings(username);
 }
 
 function paintGreetings(username){
-    greeting.innerText=`Hello ${username}`;
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
  
-if(savedUsername == null){
+if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 }else{
